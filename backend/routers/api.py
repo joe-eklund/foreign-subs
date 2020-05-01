0642036a837211af578ba692693714e230353607
Joe Eklund <eklund7@llnl.gov>
2020-04-30 18:14:07 -0700

    Reordered endpoints and added movie version endpoints.

--- backend/routers/api.py
+++ backend/routers/api.py
@@ -6,35 +6,15 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 
 from crud.movie import MovieDAO
-from models.video import VideoBase
+from models.video import VideoBase, VideoInstance
 
 app = FastAPI()
 
 MOVIE_DAO = MovieDAO(client=MongoClient('localhost', 27017, username='root', password='example'))
 
 
-@app.get("/movies/{uri}", tags=['movies'])
-async def get_movie(uri: str):
-    """
-    Get a movie.
+# /movies endpoints
 
-    **param uri** - The uri of the movie to get.
-    """
-    return MOVIE_DAO.read(movie_id=uri)
-
-# GET movie instance
-@app.get("/movies/{uri}/versions/{v_uri}", tags=['movies'], status_code=405)
-async def get_movie_version(uri: str, v_uri: str):
-    """
-    Get a movie version.
-
-    **uri** - The uri of the base movie to use.
-
-    **v_uri** - The uri of the version of the movie to get.
-    """
-    return "Not implemented yet."
-
-# POST
 @app.post("/movies", tags=['movies'], response_model=str, status_code=201)
 async def create_movie(movie: VideoBase):
     """
@@ -52,8 +32,18 @@ async def create_movie(movie: VideoBase):
     movie_to_store.Metadata.modified_by = user
     return str(MOVIE_DAO.create(movie=movie_to_store.to_dict()))
 
-# PUT
-@app.put("/movies", tags=['movies'], response_model=str, status_code=201)
+
+@app.get("/movies/{uri}", tags=['movies'])
+async def get_movie(uri: str):
+    """
+    Get a movie.
+
+    **param uri** - The uri of the movie to get.
+    """
+    return MOVIE_DAO.read(movie_id=uri)
+
+
+@app.put("/movies/{uri}", tags=['movies'], response_model=str, status_code=201)
 async def update_movie(uri: str, movie: VideoBase):
     """
     Update a movie.
@@ -71,12 +61,57 @@ async def update_movie(uri: str, movie: VideoBase):
     return str(MOVIE_DAO.update(movie_id=uri, movie=movie_to_store.to_dict()))
 
 
-# DELETE
 @app.delete("/movies/{uri}", tags=['movies'], status_code=204)
-async def delete_movie(uri):
+async def delete_movie(uri: str):
     """
     Delete a movie.
 
     **uri** - The uri of the movie to delete.
     """
     return MOVIE_DAO.delete(movie_id=uri)
+
+
+# /movies/versions endpoints
+
+@app.post("/movies/{uri}/versions", tags=['movie versions'], status_code=405)
+async def create_movie_version(uri: str, movie_version: VideoInstance):
+    """
+    Create a new movie version.
+
+    **uri** - The uri of the movie to attach the movie version to.
+
+    **movie_version** - The movie version data to create the movie version with.
+    """
+    return "Not implemented yet."
+
+
+@app.get("/movies/versions/{uri}", tags=['movie versions'], status_code=405)
+async def get_movie_version(uri: str):
+    """
+    Get a movie version.
+
+    **uri** - The uri of the version of the movie to get.
+    """
+    return "Not implemented yet."
+
+
+@app.put("/movies/versions/{uri}", tags=['movie versions'], status_code=405)
+async def update_movie_version(uri: str, movie_version: VideoInstance):
+    """
+    Update a movie version.
+
+    **uri** - The uri of the movie version of to update.
+
+    **movie_version** - The movie version data to update the movie version with.
+    """
+    return "Not implemented yet."
+
+
+@app.delete("/movies/versions/{uri}", tags=['movie versions'], status_code=405)
+async def delete_movie_version(uri: str):
+    """
+    Delete a movie version.
+
+    **uri** - The uri of the movie version to delete.
+    """
+    return "Not implemented yet."
